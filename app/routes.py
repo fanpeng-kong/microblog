f4ddcc26c7548b595960a4c3383a4ad3dd384021
Fanpeng Kong <fanpeng@fanpengkong.com>
2021-09-08 15:16:39 +1000

    Chapter 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Fanpeng'}
+    posts = [
+        {
+        'author': {'username': 'Mia'},
+        'body': 'Beautiful day in Canberra!'
+        },
+        {
+        'author': {'username': 'Coco'},
+        'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
