838e70affa02360226d01f40f0242bf9b1cff5af
Fanpeng Kong <fanpeng@fanpengkong.com>
2021-09-10 22:59:48 +1000

    Chapter 3: Web Forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.jinja_env.auto_reload = True
 
 from app import routes
+
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -8,12 +9,30 @@ def index():
     user = {'username': 'Fanpeng'}
     posts = [
         {
-        'author': {'username': 'Mia'},
-        'body': 'Beautiful day in Canberra!'
+        'author': {'username': 'Fanpeng'},
+        'body': 'Hello, Flask!'
         },
         {
         'author': {'username': 'Coco'},
         'body': 'The Avengers movie was so cool!'
+        },
+        {
+        'author': {'username': 'Mia'},
+        'body': 'Beautiful day in Canberra!'
+        },
+        {
+        'author': {'username': 'Max'},
+        'body': 'Woo Woo Waa Waa...'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
