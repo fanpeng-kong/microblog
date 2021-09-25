7d75f4837bf72a810bb29f0f480999b89622cd18
Fanpeng Kong <fanpeng@fanpengkong.com>
2021-09-26 00:29:33 +1000

    Chapter 8: Followers

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,8 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from app.models import User
-
 
 
 @app.route('/')
@@ -16,19 +15,19 @@ def index():
     posts = [
         {
         'author': {'username': 'Fanpeng'},
-        'body': 'Hello, Flask!'
+        'body': 'I love dog!'
         },
         {
         'author': {'username': 'Coco'},
-        'body': 'The Avengers movie was so cool!'
+        'body': 'I love cat!'
         },
         {
         'author': {'username': 'Mia'},
-        'body': 'Beautiful day in Canberra!'
+        'body': 'I love hamster!'
         },
         {
         'author': {'username': 'Max'},
-        'body': 'Woo Woo Waa Waa...'
+        'body': 'Woo Woo Waa Waa (I love rabbit)!'
         }
     ]
     return render_template('index.html', title='Home', posts=posts)
@@ -81,7 +80,8 @@ def user(username):
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'},
     ]
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
 
 
 @app.before_request
@@ -105,3 +105,43 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You canno follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are not following {}.'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
