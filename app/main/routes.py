8b8ad25f10d79d6c22bca3b64f45080043077534
Fanpeng Kong <fanpeng@fanpengkong.com>
2021-10-24 22:48:13 +1100

    Chapter 20: Some JavaScript Magic

--- app/main/routes.py
+++ app/main/routes.py
@@ -54,6 +54,14 @@ def user(username):
                            next_url=next_url, prev_url=prev_url, form=form)
 
 
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    form = EmptyForm()
+    return render_template('user_popup.html', user=user, form=form)
+
+
 @bp.before_app_request
 def before_request():
     if current_user.is_authenticated:
